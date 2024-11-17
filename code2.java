885bf9b05fd63f4f2770c5490d26a3e1e36b678d
Arafat <arafat.hossain1184@gmail.com>
2024-11-18 02:24:56 +0600

    first commit

---
+++ code2.java
@@ -0,0 +1,10 @@
+// Add two numbers. //
+public class code2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int sum = a + b;
+        System.out.println("Sum=" + sum);
+    }
+
+}
---
+++ code3.java
@@ -0,0 +1,22 @@
+
+// Check given number is even or odd //
+import java.util.Scanner;
+
+public class code3 {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+
+        int number = sn.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Given number is even");
+
+        }
+
+        else {
+            System.out.println("Given number is odd");
+        }
+    }
+
+}
